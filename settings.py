25d1b4a13865c1aabddaab3cf4cd53e3ba12e00e
Johannes Kröger <johannes.kroeger@wheregroup.com>
2022-10-27 14:56:06 +0200

    Mark both start and end of our string in the filter
    Closed #8

--- settings.py
+++ settings.py
@@ -1,2 +1,6 @@
 GROUP = 'MapFilter'  # The section name for filter definitions stored in QSettings
-FILTER_COMMENT = '/* MapFilter Plugin */'
+
+# The filter string might contain user-specific parts so we surround *our* filter
+# string with text markers
+FILTER_COMMENT_START = '/* MapFilter Plugin Start */'
+FILTER_COMMENT_STOP = '/* MapFilter Plugin Stop */'
